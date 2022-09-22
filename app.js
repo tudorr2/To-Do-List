c71e1b46370dcb025654f27e184660c9b970cf87
Neagu Tudor <tudorneagu7@gmail.com>
2022-09-22 21:22:04 +0300

    added the delete button

--- app.js
+++ app.js
@@ -53,10 +53,14 @@ window.addEventListener("load", () => {
         task_input_el.removeAttribute("readonly");
         task_input_el.focus();
         task_edit_el.innerText = "Save";
-      } else{
-        task_input_el.setAttribute("readonly" , "readonly");
+      } else {
+        task_input_el.setAttribute("readonly", "readonly");
         task_edit_el.innerText = "Edit";
       }
+    });
+
+    task_delete_el.addEventListener("click", () => {
+      list_el.removeChild(task_el);
     });
   });
 });
